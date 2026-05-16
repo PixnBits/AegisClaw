9f9de062f62b13d5aa19f1ebfb8a349ffe6713f5
Nick Oliver <git@pixnbits.org>
2026-05-16 12:03:34 -0700

    Add placeholder TestDaemonAPI/Auth as suggested

---
+++ cmd/aegisclaw/daemon_test.go
@@ -0,0 +1,11 @@
+package aegisclaw
+import (
+	"testing"
+	"context"
+)
+
+func TestDaemonAPI_Auth(t *testing.T) {
+	t.Parallel()
+	// TODO: Implement auth test cases for withAuthorizedCaller
+	// e.g. same-user succeeds, other-user fails
+}
